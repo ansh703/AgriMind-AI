ad5d29acbdb041617869fdc3e764ddd521563620
ansh703 <125655452+ansh703@users.noreply.github.com>
2026-07-09 23:58:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,60 @@
 import { db, auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+// Check if user is logged in
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "login.html";
   }
 });
-console.log("Firebase connected successfully!", db);// Smooth scroll for navigation
 
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(e) {
+console.log("Firebase connected successfully!", db);
 
-        if(this.getAttribute('href').startsWith("#")){
+// Smooth scroll for navigation
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    if (this.getAttribute("href").startsWith("#")) {
+      e.preventDefault();
 
-            e.preventDefault();
-
-            document.querySelector(this.getAttribute('href'))
-            .scrollIntoView({
-                behavior:'smooth'
-            });
-
-        }
-
-    });
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
 });
 
-
 // Launch Dashboard Button
-
 const btn = document.querySelector(".btn");
 
 if (btn) {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
     document.querySelector("#dashboard").scrollIntoView({
-      behavior: "smooth"
+      behavior: "smooth",
     });
   });
 }
+
+// ================= WEATHER =================
+
 const API_KEY = "2593969b2bda90149a077bf597ee0d32";
-const CITY = "Nagpur"; // Change this to your city if you want
+const CITY = "Nagpur";
 
 async function getWeather() {
   try {
-     const response = await fetch(
-  https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric
-);
+    const response = await fetch(
+      https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric
+    );
+
     const data = await response.json();
+
+    console.log(data);
 
     document.getElementById("temperature").textContent =
       data.main.temp + "°C";
 
     document.getElementById("humidity").textContent =
       data.main.humidity + "%";
-
   } catch (error) {
     console.error("Weather Error:", error);
   }

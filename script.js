29492df8a6d79c304e47d09246e799d0e7aa5d50
ansh703 <125655452+ansh703@users.noreply.github.com>
2026-07-09 23:22:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-import { db } from "./firebase.js";
+import { db, auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
 console.log("Firebase connected successfully!", db);// Smooth scroll for navigation
 
 document.querySelectorAll('nav a').forEach(link => {
